42d31da1959fc849f35d4a31dc1b1b641f66eecf
y4shiro <y4shironao@gmail.com>
2022-01-07 21:51:45 +0900

    各コンポーネントを仮置

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button: React.FC = () => {
+  return (
+    <div className="button">
+      <button>start</button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navigation: React.FC = () => {
+  return <p>navigation</p>;
+};
+
+export default Navigation;
